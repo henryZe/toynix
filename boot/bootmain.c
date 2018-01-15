88bb15402f61f3318ecd2057cfcc849b18e2d49a
henry.zeng <henry.zeng@alitech.com>
2018-01-15 19:00:22 +0800

    bootmain.c finish

---
+++ boot/bootmain.c
@@ -0,0 +1,98 @@
+/*
+ *  boot/bootmain.c
+ *
+ *  Copyright (C) 2018 Henry.Zeng <henryz_e@163.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ */
+
+#include <include/types.h>
+#include <include/x86.h>
+#include <include/elf.h>
+
+#define SECTSIZE  512
+
+void
+waitdisk(void)
+{
+	/* Wait for disk ready. */
+	while((inb(0x1F7) & 0xC0) != 0x40);
+}
+
+/* Read a single sector at offset into dst. */
+void
+readsect(void *dst, uint offset)
+{
+	/* Issue command. */
+	waitdisk();
+	outb(0x1F2, 1);   /* count = 1 */
+	outb(0x1F3, offset);
+	outb(0x1F4, offset >> 8);
+	outb(0x1F5, offset >> 16);
+	outb(0x1F6, (offset >> 24) | 0xE0);
+	outb(0x1F7, 0x20);  /* cmd 0x20 - read sectors */
+
+	/* Read data. */
+	waitdisk();
+	insl(0x1F0, dst, SECTSIZE/4);
+}
+
+/*
+ * Read 'count' bytes at 'offset' from kernel into physical address 'pa'.
+ * Might copy more than asked.
+ */
+void
+readseg(uchar *pa, uint count, uint offset)
+{
+	uchar *epa;
+
+	epa = pa + count;
+
+	/* Round down to sector boundary. */
+	pa -= offset % SECTSIZE;
+
+	/* Translate from bytes to sectors; kernel starts at sector 1. */
+	offset = (offset / SECTSIZE) + 1;
+
+	/* If this is too slow, we could read lots of sectors at a time.
+	* We'd write more to memory than asked, but it doesn't matter --
+	* we load in increasing order.
+	*/
+	for(; pa < epa; pa += SECTSIZE, offset++)
+		readsect(pa, offset);
+}
+
+void
+bootmain(void)
+{
+	struct proghdr *ph, *eph;
+	struct elfhdr *elf;
+	uchar *pa;
+	void (*entry)(void);
+
+	elf = (struct elfhdr *)0x10000;	/* scratch space */
+
+	/* Read 1st page off disk */
+	readseg((uchar *)elf, 4096, 0);
+
+	/* Is this an ELF executable? */
+	if (elf->magic != ELF_MAGIC)
+		return;
+
+	/* Load each program segment (ignores ph flags). */
+	ph = (struct proghdr *)((uchar *)elf + elf->phoff);
+	eph = ph + elf->phnum;
+	for (; ph < eph; ph++) {
+		pa = (uchar *)ph->paddr;
+		readseg(pa, ph->filesz, ph->off);
+
+		if (ph->memsz > ph->filesz)
+			stosb(pa + ph->filesz, 0, ph->memsz - ph->filesz);
+	}
+
+	/* Call the entry point from the ELF header. */
+	/* Should never return! */
+	entry = (void(*)(void))(elf->entry);
+	entry();
+}
