3e86c212068ac3146bd4727461e42105528c1171
henry.zeng <henry.zeng@alitech.com>
2018-06-04 13:19:51 +0800

    add sendpage test

--- user/primes.c
+++ user/primes.c
@@ -1,9 +1,42 @@
 #include <lib.h>
 
+unsigned
+primeproc(void)
+{
+	int i, id, p;
+	envid_t envid;
+
+	// fetch a prime from our left neighbor
+top:
+	p = ipc_recv(&envid, 0, 0);
+	cprintf("CPU %d: %d ", thisenv->env_cpunum, p);
+
+	// fork a right neighbor to continue the chain
+	if ((id = fork()) < 0)
+		panic("fork: %e", id);
+	if (id == 0)
+		goto top;
+
+	// filter out multiples of our prime
+	while (1) {
+		i = ipc_recv(&envid, 0, 0);
+		if (i % p)
+			ipc_send(id, i, 0, 0);
+	}
+}
+
 void
 umain(int argc, char **argv)
 {
+	int i, id;
 
+	// fork the first prime process in the chain
+	if ((id = fork()) < 0)
+		panic("fork: %e", id);
+	if (id == 0)
+		primeproc();
 
-
+	// feed all the integers through
+	for (i = 2; ; i++)
+		ipc_send(id, i, 0, 0);
 }
--- user/sendpage.c
+++ user/sendpage.c
@@ -1,8 +1,38 @@
 #include <lib.h>
 
+const char *str1 = "hello child environment! how are you?";
+const char *str2 = "hello parent environment! I'm good.";
+
+#define TEMP_ADDR	((char *)0xa00000)
+#define TEMP_ADDR_CHILD	((char *)0xb00000)
 
 void
 umain(int argc, char **argv)
 {
+	envid_t who;
 
+	who = fork();
+	if (who == 0) {
+		/* child */
+		ipc_recv(&who, TEMP_ADDR_CHILD, NULL);
+		cprintf("%x got message: %s\n", who, TEMP_ADDR_CHILD);
+		if (strncmp(TEMP_ADDR_CHILD, str1, strlen(str1)) == 0)
+			cprintf("child received correct message\n");
+
+		memcpy(TEMP_ADDR_CHILD, str2, strlen(str2));
+		ipc_send(who, 0, TEMP_ADDR_CHILD, PTE_W);
+		return;
+	}
+
+	/* parent */
+	sys_page_alloc(0, TEMP_ADDR, PTE_W);
+	memcpy(TEMP_ADDR, str1, strlen(str1));
+	ipc_send(who, 0, TEMP_ADDR, PTE_W);
+
+	ipc_recv(&who, TEMP_ADDR, NULL);
+	cprintf("%x got message: %s\n", who, TEMP_ADDR);
+	if (strncmp(TEMP_ADDR, str2, strlen(str2)) == 0)
+		cprintf("parent received correct message\n");
+
+	return;
 }
