54d1f2e0f6df651403b7d1321e424dcfc6af0ed9
henry.zeng <henry.zeng@alitech.com>
2018-06-19 17:00:18 +0800

    add file operations

--- lib/file.c
+++ lib/file.c
@@ -3,16 +3,58 @@
 
 #define debug 0
 
+union Fsipc fsipcbuf __attribute__((aligned(PGSIZE)));
+
+static int devfile_flush(struct Fd *fd);
+static ssize_t devfile_read(struct Fd *fd, void *buf, size_t n);
+static ssize_t devfile_write(struct Fd *fd, const void *buf, size_t n);
+static int devfile_stat(struct Fd *fd, struct Stat *stat);
+static int devfile_trunc(struct Fd *fd, off_t newsize);
+
 struct Dev devfile = {
 	.dev_id = 'f',
 	.dev_name = "file",
-/*	.dev_read = devfile_read,
+	.dev_read = devfile_read,
+	.dev_write = devfile_write,
 	.dev_close = devfile_flush,
 	.dev_stat = devfile_stat,
-	.dev_write = devfile_write,
 	.dev_trunc = devfile_trunc,
-*/
 };
 
+// Send an inter-environment request to the file server, and wait for
+// a reply.  The request body should be in fsipcbuf, and parts of the
+// response may be written back to fsipcbuf.
+// type: request code, passed as the simple integer IPC value.
+// dstva: virtual address at which to receive reply page, 0 if none.
+// Returns result from the file server.
+static int
 
+{
 
+}
+
+// Read at most 'n' bytes from 'fd' at the current position into 'buf'.
+//
+// Returns:
+// 	The number of bytes successfully read.
+// 	< 0 on error.
+static ssize_t
+devfile_read(struct Fd *fd, void *buf, size_t n)
+{
+	// Make an FSREQ_READ request to the file system server after
+	// filling fsipcbuf.read with the request arguments.  The
+	// bytes read will be written back to fsipcbuf by the file
+	// system server.
+	int ret;
+
+	fsipcbuf.read.req_fileid = fd->fd_file.id;
+	fsipcbuf.read.req_n = n;
+
+	ret = fsipc(FSREQ_READ, NULL);
+	if (ret < 0)
+		return ret;
+
+	assert(ret <= n);
+	assert(ret <= PGSIZE);
+
+}
--- user/testfile.c
+++ user/testfile.c
@@ -1,4 +1,5 @@
 #include <lib.h>
+#include <fd.h>
 
 const char *msg = "This is the NEW message of the day!\n\n";
 
@@ -44,8 +45,44 @@ umain(int argc, char **argv)
 
 	cprintf("serve_open is good\n");
 
+	ret = devfile.dev_stat(FVA, &st);
+	if (ret < 0)
+		panic("file_stat: %e", ret);
 
+	if (strlen(msg) != st.st_size)
+		panic("file_stat returned size %d wanted %d\n", st.st_size, strlen(msg));
 
+	cprintf("file_stat is good\n");
+
+	memset(buf, 0, sizeof(buf));
+	ret = devfile.dev_read(FVA, buf, sizeof(buf));
+	if (ret < 0)
+		panic("file_read: %e", ret);
+	if (strcmp(buf, msg))
+		panic("file_read returned wrong data");
+
+	cprintf("file_read is good\n");
+
+	ret = devfile.dev_close(FVA);
+	if (ret < 0)
+		panic("file_close: %e", ret);
+
+	cprintf("file_close is good\n");
+
+	// We're about to unmap the FD, but still need a way to get
+	// the stale filenum to serve_read, so we make a local copy.
+	// The file server won't think it's stale until we unmap the
+	// FD page.
+	fdcopy = *FVA;
+	sys_page_unmap(0, FVA);
+
+	ret = devfile.dev_read(&fdcopy, buf, sizeof(buf));
+	if (ret != -E_INVAL)
+		panic("serve_read does not handle stale fileids correctly: %e", ret);
+
+	cprintf("stale fileid is good\n");
+
+	// Try writing
 
 
 }
