079f6b2de27da0016d3155db8f0084d00285ca5c
henry <henryz_e@163.com>
2019-08-14 13:40:23 +0800

    add math

---
+++ include/math.h
@@ -0,0 +1,22 @@
+#ifndef INC_MATH_H
+#define INC_MATH_H
+
+#define is_pow_of_2(x) (!((x) & ((x) - 1)))
+
+static inline unsigned long next_pow_of_2(unsigned long x)
+{
+	if (is_pow_of_2(x))
+		return x;
+
+	x |= x>>1;
+	x |= x>>2;
+	x |= x>>4;
+	x |= x>>8;
+	x |= x>>16;
+
+	return x + 1;
+}
+
+unsigned long log_of_2(unsigned long number);
+
+#endif
---
+++ lib/math.c
@@ -0,0 +1,17 @@
+/* File for math functions */
+
+#include <math.h>
+
+/* calc the log base 2 of a number */
+unsigned long log_of_2(unsigned long number)
+{
+	static unsigned long MultiplyDeBruijnBitPosition2[32] = {
+		0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+		31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+	};
+
+	if (!is_pow_of_2(number))
+		number = next_pow_of_2(number) >> 1;
+
+	return MultiplyDeBruijnBitPosition2[((number * 0x077CB531UL) >> 27) & 0x1F];
+}
